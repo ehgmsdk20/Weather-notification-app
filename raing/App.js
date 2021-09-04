86c3df0f214fbf6c6739264d91fcd369455b6694
박준하 <csjunha@bagjunhas-MacBook-Pro.local>
2021-09-05 01:02:49 +0900

    failed

--- raing/App.js
+++ raing/App.js
@@ -6,190 +6,29 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {
-  TextInput,
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  Alert,
-} from 'react-native';
-import NotifService from './NotifService';
+import React, {Component, useEffect} from 'react';
+import HeadlessWorkManager from 'react-native-headless-work-manager'
+import {Text} from 'react-native'
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
+const App = () => {
+  useEffect(() => {
+    HeadlessWorkManager.enqueue({
+      workRequestType: HeadlessWorkManager.PERIODIC_WORK_REQUEST,
+				taskKey: 'YourHeadlessTask',
+				isUnique: true,
+				existingWorkPolicy: HeadlessWorkManager.REPLACE,
+				timeout: 1000,
+				interval: 100,
+				timeUnit: HeadlessWorkManager.MILLISECONDS,
+				data: {
+					foo: 'bar',
+				}
+    });
+  }, [])
 
-    this.notif = new NotifService(
-      this.onRegister.bind(this),
-      this.onNotif.bind(this),
-    );
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.title}>
-          Example app react-native-push-notification
-        </Text>
-        <View style={styles.spacer}></View>
-        <TextInput
-          style={styles.textField}
-          value={this.state.registerToken}
-          placeholder="Register token"
-        />
-        <View style={styles.spacer}></View>
-
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            this.notif.localNotif();
-          }}>
-          <Text>Local Notification (now)</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            this.notif.localNotif('sample.mp3');
-          }}>
-          <Text>Local Notification with sound (now)</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            this.notif.scheduleNotif();
-          }}>
-          <Text>Schedule Notification in 30s</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            this.notif.scheduleNotif('sample.mp3');
-          }}>
-          <Text>Schedule Notification with sound in 30s</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            this.notif.cancelNotif();
-          }}>
-          <Text>Cancel last notification (if any)</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            this.notif.cancelAll();
-          }}>
-          <Text>Cancel all notifications</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            this.notif.checkPermission(this.handlePerm.bind(this));
-          }}>
-          <Text>Check Permission</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            this.notif.requestPermissions();
-          }}>
-          <Text>Request Permissions</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            this.notif.abandonPermissions();
-          }}>
-          <Text>Abandon Permissions</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            this.notif.getScheduledLocalNotifications(notifs => console.log(notifs));
-          }}>
-          <Text>Console.Log Scheduled Local Notifications</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            this.notif.getDeliveredNotifications(notifs => console.log(notifs));
-          }}>
-          <Text>Console.Log Delivered Notifications</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            this.notif.createOrUpdateChannel();
-          }}>
-          <Text>Create or update a channel</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            this.notif.popInitialNotification();
-          }}>
-          <Text>popInitialNotification</Text>
-        </TouchableOpacity>
-
-        <View style={styles.spacer}></View>
-
-        {this.state.fcmRegistered && <Text>FCM Configured !</Text>}
-
-        <View style={styles.spacer}></View>
-      </View>
-    );
-  }
-
-  onRegister(token) {
-    this.setState({registerToken: token.token, fcmRegistered: true});
-  }
-
-  onNotif(notif) {
-    Alert.alert(notif.title, notif.message);
-  }
-
-  handlePerm(perms) {
-    Alert.alert('Permissions', JSON.stringify(perms));
-  }
+  return (
+    <Text> HI </Text>
+  );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  button: {
-    borderWidth: 1,
-    borderColor: '#000000',
-    margin: 5,
-    padding: 5,
-    width: '70%',
-    backgroundColor: '#DDDDDD',
-    borderRadius: 5,
-  },
-  textField: {
-    borderWidth: 1,
-    borderColor: '#AAAAAA',
-    margin: 5,
-    padding: 5,
-    width: '70%',
-  },
-  spacer: {
-    height: 10,
-  },
-  title: {
-    fontWeight: 'bold',
-    fontSize: 20,
-    textAlign: 'center',
-  },
-});
+export default App;
--- raing/index.js
+++ raing/index.js
@@ -5,7 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import PushNotification from 'react-native-push-notification'
 
-
+AppRegistry.registerHeadlessTask('YourHeadlessTask', () => async data => console.log(data));
 AppRegistry.registerComponent(appName, () => App);
