478962ca0459010b35684c47e3ae433850635d49
박준하 <csjunha@bagjunhas-MacBook-Pro.local>
2021-09-05 18:53:09 +0900

    fuck

--- raing/App.js
+++ raing/App.js
@@ -6,29 +6,127 @@
  * @flow
  */
 
-import React, {Component, useEffect} from 'react';
-import HeadlessWorkManager from 'react-native-headless-work-manager'
-import {Text} from 'react-native'
+import React, {useState} from 'react';
+
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+    TouchableOpacity,
+    Platform,
+    Linking,
+} from 'react-native';
+
+import BackgroundServer from 'react-native-background-actions';
+
+// You can do anything in your task such as network requests, timers and so on,
+// as long as it doesn't touch UI. Once your task completes (i.e. the promise is resolved),
+// React Native will go into "paused" mode (unless there are other tasks running,
+// or there is a foreground app).
+
+const sleep = (time) => new Promise((resolve) => setTimeout(() => resolve(), time))
+
+const veryIntensiveTask = async (taskDataArguments) => {
+    // Example of an infinite loop task
+    const { delay } = taskDataArguments;
+    await new Promise( async (resolve) => {
+        for (let i = 0; BackgroundServer.isRunning(); i++) {
+            await BackgroundServer.updateNotification({ taskDesc: 'Runned -> ' + i });
+            await sleep(delay);
+        }
+    });
+};
+
+const options = {
+    taskName: 'Example',
+    taskTitle: 'ExampleTask title',
+    taskDesc: 'ExampleTask description',
+    taskIcon: {
+        name: 'ic_launcher',
+        type: 'mipmap',
+    },
+    color: '#ff00ff',
+    parameters: {
+        delay: 1000,
+    },
+};
+
+const styles = StyleSheet.create({
+    scrollView: {
+        backgroundColor: Colors.lighter,
+    },
+    engine: {
+        position: 'absolute',
+        right: 0,
+    },
+    body: {
+        backgroundColor: Colors.white,
+    },
+    footer: {
+        color: Colors.dark,
+        fontSize: 12,
+        fontWeight: '600',
+        padding: 4,
+        paddingRight: 12,
+        textAlign: 'right',
+    },
+});
 
 const App = () => {
-  useEffect(() => {
-    HeadlessWorkManager.enqueue({
-      workRequestType: HeadlessWorkManager.PERIODIC_WORK_REQUEST,
-				taskKey: 'YourHeadlessTask',
-				isUnique: true,
-				existingWorkPolicy: HeadlessWorkManager.REPLACE,
-				timeout: 1000,
-				interval: 100,
-				timeUnit: HeadlessWorkManager.MILLISECONDS,
-				data: {
-					foo: 'bar',
-				}
-    });
-  }, [])
 
-  return (
-    <Text> HI </Text>
-  );
+    const [isRunning, setIsRunning] = useState(BackgroundServer.isRunning());
+
+    const startBackgroundTask = async () => {
+        if (!isRunning) {   
+            try {
+                console.log('Trying to start background service...');
+                await BackgroundServer.start(veryIntensiveTask, options);
+                console.log('Success');
+                setIsRunning(true);
+            } catch (e) {
+                console.log('Error ', e);
+            }
+        }
+    };
+
+    const stopBackgroundTask = async () => {
+        if(isRunning) {
+            try {
+                console.log('Trying to terminate background service...');
+                await BackgroundServer.stop();
+                console.log('Success');
+                setIsRunning(false);
+            } catch (e) {
+                console.log('Error ', e);
+            }
+        }
+    };
+    
+    return (
+        <>
+            <StatusBar barStyle="dark-content" />
+            <SafeAreaView>
+                <ScrollView
+                    contentInsetAdjustmentBehavior="automatic"
+                    style={styles.scrollView}>
+                    <Header />
+                    {global.HermesInternal == null ? null : (
+                        <View style={styles.engine}>
+                            <Text style={styles.footer}>Engine: Hermes</Text>
+                        </View>
+                    )}
+                    <View style={styles.body}>
+                        <TouchableOpacity
+                            style={{ height: 100, width: 100, backgroundColor: 'red' }}
+                            onPress={startBackgroundTask}/>
+                    </View>
+                </ScrollView>
+            </SafeAreaView>
+        </>
+    );
 }
 
 export default App;
--- raing/index.js
+++ raing/index.js
@@ -6,5 +6,4 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerHeadlessTask('YourHeadlessTask', () => async data => console.log(data));
 AppRegistry.registerComponent(appName, () => App);
